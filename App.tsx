2e25e2407ff1c9c6550b928ba215ac20490eb96b
Nathan M <nathanmayall@icloud.com>
2021-01-16 23:47:13 +0000

    still no joy with this map method

--- App.tsx
+++ App.tsx
@@ -11,21 +11,20 @@ import { Post } from './types';
 Axios.defaults.baseURL = 'http://localhost:5000/api';
 Axios.defaults.withCredentials = true;
 
-export default function App() {
-  const postList = async () => {
-    const { data: posts } = await Axios.get('/posts');
-    posts?.map((post: Post) => {
-      <View key={post.identifier} style={{ margin: 10 }}>
-        <Text>{post.title}</Text>
-        <Text>{post?.body}</Text>
-      </View>;
-    });
-  };
+export default async function App() {
+  const { data: posts } = await Axios.get('/posts');
 
   return (
     <SafeAreaView style={tailwind('h-full container')}>
       <Header />
-      <View style={tailwind('items-center flex')}>{postList()}</View>
+      <View style={tailwind('items-center flex')}>
+        {posts.map((p: Post) => {
+          <View key={p.identifier} style={{ margin: 10 }}>
+            <Text>{p.title}</Text>
+            <Text>{p?.body}</Text>
+          </View>;
+        })}
+      </View>
     </SafeAreaView>
   );
 }
