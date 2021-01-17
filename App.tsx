dd33101544e7539fcdb4acd217dca8be0337c7fc
Nathan M <nathanmayall@icloud.com>
2021-01-17 00:19:29 +0000

    Revert "Add header and custom fonts"
    This reverts commit e8d3aea4336be3118ebbb55a633c9e5170846fb1.

--- App.tsx
+++ App.tsx
@@ -1,34 +1,16 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 import tailwind from 'tailwind-rn';
-
-<<<<<<< HEAD
 import Axios from 'axios';
 
-import { SafeAreaView, Text, View } from 'react-native';
-import { tailwind } from './lib/tailwind';
-import Header from './components/Header';
-import { Post } from './types';
+// import { Post } from './types';
 
 Axios.defaults.baseURL = 'http://localhost:5000/api';
 Axios.defaults.withCredentials = true;
 
 export default async function App() {
-  const { data: posts } = await Axios.get('/posts');
+  // const { data: posts } = await Axios.get('/posts');
 
-  return (
-    <SafeAreaView style={tailwind('h-full container')}>
-      <Header />
-      <View style={tailwind('items-center flex')}>
-        {posts.map((p: Post) => {
-          <View key={p.identifier} style={{ margin: 10 }}>
-            <Text>{p.title}</Text>
-            <Text>{p?.body}</Text>
-          </View>;
-        })}
-=======
-export default function App() {
   return (
     <SafeAreaView
       style={tailwind('flex-1 items-center justify-center')}
@@ -37,7 +19,6 @@ export default function App() {
         <Text style={tailwind('text-white font-semibold text-lg')}>
           Hello Tailwind 👋
         </Text>
->>>>>>> parent of e8d3aea... Add header and custom fonts
       </View>
     </SafeAreaView>
   );
