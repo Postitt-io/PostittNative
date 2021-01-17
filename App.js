95ceb7db6f73491fa4e07e2cecd809cf1772a171
Nathan M <nathanmayall@icloud.com>
2021-01-17 19:30:05 +0000

    improve axios postfetch

--- App.js
+++ App.js
@@ -1,50 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, ActivityIndicator, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
 import Header from './components/Header';
 import PostList from './components/PostList';
-import axios from 'axios';
-import tailwind from 'tailwind-rn';
-
-axios.defaults.baseURL = 'http://10.0.1.5:5000/api';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      dataSource: null,
-    };
-  }
-
-  componentDidMount() {
-    return axios
-      .get('/posts')
-      .then((res) => {
-        this.setState({
-          isLoading: false,
-          posts: res.data,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={tailwind('pt-5 px-5 flex bg-white')}>
-          <ActivityIndicator />
-        </View>
-      );
-    } else {
-      return (
-        <SafeAreaView>
-          <Header />
-          <PostList data={this.state.posts} />
-        </SafeAreaView>
-      );
-    }
+    return (
+      <SafeAreaView>
+        <Header />
+        <PostList />
+      </SafeAreaView>
+    );
   }
 }
--- components/PostList.js
+++ components/PostList.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import { tailwind } from '../lib/tailwind';
+import axios from 'axios';
 
-export default function PostList({ data }) {
+axios.defaults.baseURL = 'http://10.0.1.5:5000/api';
+
+export default function PostList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      axios
+        .get('/posts')
+        .then((res) => {
+          setPosts(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    getPosts();
+  }, []);
+
   function Item({ title, body, subName }) {
     return (
       <TouchableOpacity style={tailwind('bg-blue-400 p-3 rounded-lg items-center m-1')}>
@@ -15,11 +34,10 @@ export default function PostList({ data }) {
       </TouchableOpacity>
     );
   }
-
   return (
     <View style={tailwind('items-center')}>
       <FlatList
-        data={data}
+        data={posts}
         keyExtractor={(item) => item.identifier}
         renderItem={({ item }) => (
           <Item title={item.title} body={item.body} subName={item.subName} />
